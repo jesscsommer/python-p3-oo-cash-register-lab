44cbd6351c0afa9c5ca6a954ddf3dfb83d9173ac
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-25 16:17:44 -0700

    Fix indentation

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -2,33 +2,32 @@
 
 class CashRegister:
 
-  def __init__(self, discount = 0):
-    self.discount = discount
-    self.total = 0
-    self.items = []
-    self.last_transaction = {
-      'items': [], 
-      'subtotal': 0
-    }
-  
-  def add_item(self, title, price, quantity = 1):
-    self.last_transaction['items'] = []
-    for _ in range(quantity):
-      self.items.append(title)
-      self.last_transaction.get('items').append('title')
-    self.last_transaction['subtotal'] = price * quantity
-    self.total += (price * quantity)
+    def __init__(self, discount = 0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.last_transaction = {
+        'items': [], 
+        'subtotal': 0
+        }
+
+    def add_item(self, title, price, quantity = 1):
+        self.last_transaction['items'] = []
+        for _ in range(quantity):
+            self.items.append(title)
+        self.last_transaction.get('items').append(self.items)
+        self.last_transaction['subtotal'] = price * quantity
+        self.total += (price * quantity)
     
 
-  def apply_discount(self):
-    if self.discount: 
-      self.total *= (1 - (self.discount / 100))
-      print(f'After the discount, the total comes to ${int(self.total)}.')
-    else: 
-      print('There is no discount to apply.')
+    def apply_discount(self):
+        if self.discount: 
+            self.total *= (1 - (self.discount / 100))
+            print(f'After the discount, the total comes to ${int(self.total)}.')
+        else: 
+            print('There is no discount to apply.')
 
-  def void_last_transaction(self):
-    self.items = [item for item in self.items 
-                  if (item not in self.last_transaction.get('items'))]
-    self.total -= self.last_transaction.get('subtotal')
-    
+    def void_last_transaction(self):
+        self.items = [item for item in self.items 
+                        if (item not in self.last_transaction.get('items'))]
+        self.total -= self.last_transaction.get('subtotal')
